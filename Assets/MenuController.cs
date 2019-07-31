2dd3606a9be4c52d5fb9ccf017c9e322b46d76e0
krackd <43828895+krackd@users.noreply.github.com>
2019-07-31 23:24:52 +0200

    Enhanced Main Menu + new levels

--- Assets/MenuController.cs
+++ Assets/MenuController.cs
@@ -3,6 +3,58 @@ using UnityEngine.SceneManagement;
 
 public class MenuController : MonoBehaviour {
 
+	private GameObject mainMenu;
+	private GameObject puzzleMenu;
+	private GameObject versusMenu;
+	private GameObject backButton;
+
+	private void Start()
+	{
+		mainMenu = GameObject.Find("/Canvas/MainMenu");
+		puzzleMenu = GameObject.Find("/Canvas/Puzzle");
+		versusMenu = GameObject.Find("/Canvas/Versus");
+		backButton = GameObject.Find("/Canvas/Back");
+
+		ShowMainMenu();
+	}
+
+	public void ShowMainMenu()
+	{
+		HideAll();
+		mainMenu.SetActive(true);
+	}
+
+	public void ShowPuzzleMenu()
+	{
+		HideAll();
+		puzzleMenu.SetActive(true);
+		ShowBackButton();
+	}
+	public void ShowVersusMenu()
+	{
+		HideAll();
+		versusMenu.SetActive(true);
+		ShowBackButton();
+	}
+
+	private void HideAll()
+	{
+		mainMenu.SetActive(false);
+		puzzleMenu.SetActive(false);
+		versusMenu.SetActive(false);
+		HideBackButton();
+	}
+
+	private void HideBackButton()
+	{
+		backButton.SetActive(false);
+	}
+	
+	private void ShowBackButton()
+	{
+		backButton.SetActive(true);
+	}
+
 	public void LoadScene(string sceneName)
 	{
 		SceneManager.LoadScene(sceneName);
