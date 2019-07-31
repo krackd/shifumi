61eb52e08f3c67c480f08c75cb9df482c6c301f4
krackd <43828895+krackd@users.noreply.github.com>
2019-07-31 13:11:08 +0200

    Main Menu

---
+++ Assets/MenuController.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuController : MonoBehaviour {
+
+	public void LoadScene(string sceneName)
+	{
+		SceneManager.LoadScene(sceneName);
+	}
+
+	public void ExitGame()
+	{
+		Application.Quit();
+	}
+}
